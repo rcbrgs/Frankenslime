f02ea33149adb08de17675be357e76934def8c97
Renato Callado Borges <rborges@hush.com>
2018-11-12 15:50:17 -0200

    Added spit attack.

---
+++ Frankenslime/Enemies/Skeleton.gd
@@ -0,0 +1,4 @@
+extends KinematicBody2D
+
+func remove_hp(damage):
+	$Enemy/Health.remove_hp(damage)
--- Frankenslime/Player/Player.gd
+++ Frankenslime/Player/Player.gd
@@ -1,13 +1,10 @@
 extends KinematicBody2D
 
+export (float) var attack_interval = 1.5
 export (int) var horizontal_speed = 5
 export (int) var vertical_speed = 2
 
-func _ready():
-	pass
-
-func _process(delta):
-	pass
+onready var spit_bullet_scene = preload("res://Bullets/SpitBullet.tscn")
 
 func _physics_process(delta):
 	var motion = Vector2()
@@ -27,3 +24,17 @@ func _physics_process(delta):
 	var scene = get_parent().get_node("SceneParameters")
 	position.x = clamp(position.x, scene.min_x, scene.max_x)
 	position.y = clamp(position.y, scene.min_y, scene.max_y)
+	
+	# Attack
+	if Input.is_action_pressed("ui_select"):
+		if $AttackTimer.is_stopped():
+			launch_attack()
+			$AttackTimer.set_wait_time(attack_interval)
+			$AttackTimer.start()
+	
+func launch_attack():
+	#print("launch_attack")
+	var bullet = spit_bullet_scene.instance()
+	get_parent().add_child(bullet)
+	bullet.set_initial_position(position)
+	bullet.set_collision_layer_bit(3, 8) # set layer as BulletsFromPlayer
