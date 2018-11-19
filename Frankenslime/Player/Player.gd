bcba6da5a9909103a895dcc3098e22979d642d37
opleks <opleks@gmx.ch>
2018-11-20 00:02:48 +0100

    implemented basic functionality of animationplayer controlling animatedsprite, backup due to code issues in input

--- Frankenslime/Player/Player.gd
+++ Frankenslime/Player/Player.gd
@@ -14,7 +14,9 @@ export (int) var vertical_speed = 10
 onready var bone_bullet_wrapper_scene = preload("res://Bullets/BoneBulletWrapper.tscn")
 onready var spit_bullet_scene = preload("res://Bullets/SpitBullet.tscn")
 onready var HP = max_HP
-onready var anim_state = "none"
+
+onready var anim_node = get_node("AnimatedSprites/Animator")
+onready var anim_state = ""
 
 var dead = false
 var h_flipper = false
@@ -23,14 +25,11 @@ var weapon_node = null
 var melee_active = false
 
 var min_save_pos = 0 # The leftmost position of the walkable window
-var min_last_pos = 0
 var motion = Vector2()
 onready var scene = get_parent().get_node("SceneParameters")
 	
 func _ready():
 	emit_signal("changed_player_hp", HP, max_HP)
-	
-	change_animation("hop")
 
 func get_input():
 	motion = Vector2(0,0)
@@ -39,29 +38,28 @@ func get_input():
 	if Input.is_action_pressed("ui_right"):
 		motion.x = horizontal_speed
 		h_flipper = false
-		change_animation("hop")
+		anim_state = "hop"
 	if Input.is_action_pressed("ui_left"):
 		motion.x = -horizontal_speed
 		h_flipper = true
-		change_animation("hop")
+		anim_state = "hop"
 	if Input.is_action_pressed("ui_up"):
 		motion.y = -vertical_speed
-		change_animation("hop")
+		anim_state = "hop"
 	if Input.is_action_pressed("ui_down"):
 		motion.y = vertical_speed
-		change_animation("hop")
+		anim_state = "hop"
 	if Input.is_action_pressed("action_jump"):
 		jump()
 	if Input.is_action_pressed("action_shoot"):
 		if $AttackTimer.is_stopped():
+			anim_state = "spit"
 			launch_attack()
 			var attack_interval = spit_attack_interval
 			if weapon_node != null:
 				attack_interval = weapon_node.fire_interval
 			$AttackTimer.set_wait_time(attack_interval)
 			$AttackTimer.start()
-
-func _physics_process(delta):
 	get_input()
 	set_facing()
 	var collision = move_and_collide(motion)
@@ -105,9 +103,7 @@ func camera_and_lookback():
 	# (instead of directly after turning left from middle of screen)
 	if self.position.x + (get_viewport().size.x / 2) > min_save_pos and (position.x - (get_viewport().size.x / 2) > min_save_pos):
 		min_save_pos = self.position.x - (get_viewport().size.x / 2)
-		min_last_pos = self.position.x
 	#print ("min_save_pos: " + str(min_save_pos) + " min_last_pos: " +  str(min_last_pos) + " player position: " + str(position.x))
-	min_last_pos = int(min_last_pos)
 	get_node("Camera2D").limit_left = min_save_pos
 	
 	#adding some 45px threshold offsets to the right in order to trigger conditionals walking to left screen edge.
@@ -140,7 +136,8 @@ func launch_attack():
 			$MeleeTimer.start()
 	
 func set_facing():
-	get_node("BodySprite").flip_h = h_flipper
+
+	get_node("AnimatedSprites").flip_h = h_flipper
 	if weapon_node != null:
 		weapon_node.get_node("BodySprite").flip_h = h_flipper
 	
@@ -174,11 +171,6 @@ func jump():
 		jump_y = position.y - jump_height
 		jump_y = clamp(jump_y, scene.min_y, scene.max_y)
 		jumping = true
-
-onready var anim_node = get_node("BodySprite/AnimationPlayer")
-
-func change_animation(anim_state):	
-	if anim_state != anim_node.current_animation:
-		anim_node.play(anim_state)
 	
-	
+func _process(delta):
+		anim_node.change_animation("hop", anim_node)
